ff72de166420975291c48b6f17a66382802e1723
mahmoud.aboelenien <mahmoud.aboelenien@sdtgroup.co>
2026-08-24 13:59:45 +0300

    feat(navbar):build navbar

--- src/app/layout/user/user.ts
+++ src/app/layout/user/user.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { USER_NAVIGATION } from '@app/shared/constants/navigation.constants';
+import { Heart } from '@primeicons/angular/heart';
+import { ShoppingCart } from '@primeicons/angular/shopping-cart';
+import { Times } from '@primeicons/angular/times';
+import { Bars } from '@primeicons/angular/bars';
 
 @Component({
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, Heart, ShoppingCart, Times, Bars],
   selector: 'app-user',
   styleUrl: './user.css',
   templateUrl: './user.html',
 })
-export class User {}
+export class UserLayout {
+  cartCount = signal(0); // replace with real cart service signal
+  favCount = signal(0); // replace with real favorites service signal
+  private userNavigationList = signal(USER_NAVIGATION);
+
+  userNavigationItems = computed(() => this.userNavigationList());
+
+  mobileOpen = signal(false);
+
+  toggleMenu() {
+    this.mobileOpen.update((v) => !v);
+  }
+  closeMenu() {
+    this.mobileOpen.set(false);
+  }
+}
---
+++ src/app/shared/constants/navigation.constants.ts
@@ -0,0 +1,5 @@
+export const USER_NAVIGATION = [
+  { label: 'home', route: '/users' },
+  { label: 'products', route: '/users/products' },
+  { label: 'about', route: '/users/about' },
+];
